50536003e2234daa045498f4d7b77a4c2d2e0acb
HarimKim <lklkcmk@naver.com>
2017-07-26 17:19:15 +0900

    1
    1

---
+++ Assets/scripts/Map/moveBack1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBack1 : MonoBehaviour
+{
+    public GameObject background;
+    public float speed = 1f;
+
+    private void Start()
+    {
+        background.transform.position = new Vector3(43.64f, 7.65f, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        background.transform.position -= new Vector3(speed * Time.deltaTime * (1), 0, 0);
+        if (background.transform.position.x < -40.0f)
+        {
+
+            background.transform.position = new Vector3(120f, 7.65f, 0);
+        }
+
+    }
+}
---
+++ Assets/scripts/players/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float speed = 0.5f; //속도
+    private Transform playersTf; //참조를 위해
+    private Vector3 playerPos; //플레이어 위치값
+
+    void Awake()
+    {
+        playersTf = transform; //현재 스크립트 가지고 있는 자신의 transform
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        playerPos = playersTf.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        playerPos.x += speed;
+        playersTf.position = playerPos;
+	}
+}
