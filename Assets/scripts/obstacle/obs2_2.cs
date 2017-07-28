f97682b7100abb5910e1b3e2600f7a436548cf9c
Semi Moon <21400270@handong.edu>
2017-07-28 17:36:29 +0900

    project

---
+++ Assets/scripts/obstacle/obs2_2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obs2_2 : MonoBehaviour
+{
+    public GameObject obs;
+    public float speed = 7.2f;
+    private void Start()
+    {
+        obs.transform.position = new Vector3(82.69f, 1.08f, 0);
+
+    }
+    void Update()
+    {
+
+        obs.transform.position -= new Vector3(speed * Time.deltaTime * (1), 0, 0);
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("충돌 : " + col.name);
+    }
+}
---
+++ Assets/scripts/obstacle/obs3_3.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obs3_3 : MonoBehaviour
+{
+    public GameObject obs;
+    public float speed = 7.2f;
+    private void Start()
+    {
+        obs.transform.position = new Vector3(270.2f, 2.58f, 0);
+
+    }
+    void Update()
+    {
+
+        obs.transform.position -= new Vector3(speed * Time.deltaTime * (1), 0, 0);
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("충돌 : " + col.name);
+    }
+}
